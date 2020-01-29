7adb0319ab70c8a9b9f8c59bbf666ae2a5a2f673
kympeters1 <51617357+kympeters1@users.noreply.github.com>
2020-01-29 13:55:57 +0000

    Add files via upload

---
+++ build/precache-manifest.4ddc6549a900b07aa4a0e54bebf1d9d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "260f0ec680d596f3da9b7670ac67b7d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dba1d4c36d8081effe3d",
+    "url": "/static/css/main.266173d9.chunk.css"
+  },
+  {
+    "revision": "6cbcdbee3ce28adc3be0",
+    "url": "/static/js/2.cefd34a5.chunk.js"
+  },
+  {
+    "revision": "f4dd3ea946eda1151bd0ef4fc95a28d5",
+    "url": "/static/js/2.cefd34a5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dba1d4c36d8081effe3d",
+    "url": "/static/js/main.3629f1c3.chunk.js"
+  },
+  {
+    "revision": "51f36ed497346e7e42d9",
+    "url": "/static/js/runtime-main.7d422a29.js"
+  }
+]);
---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+
+const NavBar = () => <AppBar title="Picture Finder" />;
+
+
+
+
+export default NavBar;
